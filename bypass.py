178d699b0e292e9e9922ab00958c1e403d6e3ce5
michaelxiao7 <44096341+michaelxiao7@users.noreply.github.com>
2020-09-09 11:52:32 -0400

    Update bypass.py
    Edited code to continuously run in the background, and schedule a time to routinely do the wellness check

--- bypass.py
+++ bypass.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """It's simple. We all hate the Gordon Wellness check ROFL"""
 
 import time
+import schedule
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 # DON'T SHARE THESE WITH OTHER PEOPLE!
-USERNAME = "EDIT THIS VALUE" #first.last@gordon.edu
-PASSWORD = "EDIT THIS VALUE"
+USERNAME = "WRITE YOUR USERNAME INSIDE THESE QUOTES"
+PASSWORD = "WRITE YOUR PASSWORD INSIDE THESE QUOTES"
+RETRY = 0
 
 def click_button():
     """The main function :D"""
     # Drivers
     driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.set_window_position(-10000, 10000)
     driver.set_page_load_timeout("5")
     driver.get("https://360.gordon.edu/")
     try:
@@ -32,7 +35,14 @@ def click_button():
         driver.find_element_by_xpath("//span[text()='Submit']").click()
         time.sleep(1)
         driver.quit()
+        RETRY = 0
     except:
-        print("Something went wrong: Check your credentials or if your browser timed out")
+        if retry < 2:
+            click_button()
+            RETRY+=1
 
-click_button()
+schedule.every().day.at("07:00:02").do(click_button) #You can change this time to whatever you would like (HH:MM:SS)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
